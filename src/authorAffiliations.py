6a0c2463e1f2ca93b286b9746c3fa1e90c2c5049
Dent Earl <dearl@soe.ucsc.edu>
2012-01-17 13:38:08 -0800

    added boilerplate info and license information to source.

--- src/authorAffiliations.py
+++ src/authorAffiliations.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python2.6
+""" 
+authorAffiliations.py
+17 Jan 2012
+dent earl
+
+Script to help create a superscripted author list
+from an affiliation file that contains small groups 
+of authors and affiliations. See example/ for an
+example input. See README.md for a short explanation.
+
 """
-"""
+##############################
+# Copyright (C) 2011-2012 by 
+# Dent Earl (dearl@soe.ucsc.edu, dentearl@gmail.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+##############################
 from optparse import OptionParser
 import os
 import re
